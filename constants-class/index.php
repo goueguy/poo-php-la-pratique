ef619915d5d54574d4d03d448949a6ab7ad9c6d3
goueguy <jlagoueguy@gmail.com>
2021-04-19 15:52:19 +0000

    static attribute and methods

--- constants-class/index.php
+++ constants-class/index.php
@@ -2,10 +2,28 @@
     
     class Facture{
         const TVA = 20;
-        public function montantTTC($ht){
+        private static $attribute;
+        public function __construct(){
+            self::$attribute="PDO";
+        }
+        public static function montantTTC($ht){
             return $ht+(1+self::TVA/100);
         }
+        //GETTER
+        public function afficherAttribute(){
+            return self::$attribute;
+        }
+        //SETTER
+        public function setAttribute($attribute){
+            self::$attribute = $attribute;
+        }
     }
-    $facture = new Facture();
-    echo $facture->montantTTC(1000);
+    $ob = new Facture();
+    $ob2 = new Facture();
+    $ob->setAttribute("MYSQL");
+    echo "<br>";
+    echo $ob->afficherAttribute();
+    echo "<br>";
+    $ob->setAttribute("MongoDB");
+    echo $ob2->afficherAttribute();
 ?>
